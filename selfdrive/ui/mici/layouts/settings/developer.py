59e4f4cb5b4c68ca565a42a9c36be066530ddd55
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2026-04-21 14:03:24 +0200

    CAN streaming toggle+

--- selfdrive/ui/mici/layouts/settings/developer.py
+++ selfdrive/ui/mici/layouts/settings/developer.py
@@ -1,3 +1,8 @@
+import os
+import signal
+import subprocess
+
+from openpilot.common.basedir import BASEDIR
 from openpilot.common.time_helpers import system_time_valid
 from openpilot.system.ui.widgets.scroller import NavScroller
 from openpilot.selfdrive.ui.mici.widgets.button import BigButton, BigToggle, BigParamControl, BigCircleParamControl
@@ -7,6 +12,8 @@ from openpilot.selfdrive.ui.layouts.settings.common import restart_needed_callba
 from openpilot.selfdrive.ui.ui_state import ui_state
 from openpilot.selfdrive.ui.widgets.ssh_key import SshKeyFetcher
 
+BRIDGE_PATH = os.path.join(BASEDIR, "cereal", "messaging", "bridge")
+_bridge_proc: subprocess.Popen | None = None
 
 class DeveloperLayoutMici(NavScroller):
   def __init__(self):
@@ -52,6 +59,9 @@ class DeveloperLayoutMici(NavScroller):
     self._joystick_toggle = BigToggle("joystick debug mode",
                                       initial_state=ui_state.params.get_bool("JoystickDebugMode"),
                                       toggle_callback=self._on_joystick_debug_mode)
+    self._bridge_toggle = BigToggle("CAN streaming",
+                                      initial_state=_bridge_proc is not None and _bridge_proc.poll() is None,
+                                      toggle_callback=self._on_start_can_bridge)
     self._long_maneuver_toggle = BigToggle("longitudinal maneuver mode",
                                            initial_state=ui_state.params.get_bool("LongitudinalManeuverMode"),
                                            toggle_callback=self._on_long_maneuver_mode)
@@ -70,6 +80,7 @@ class DeveloperLayoutMici(NavScroller):
       self._ssh_toggle,
       self._ssh_keys_btn,
       self._joystick_toggle,
+      self._bridge_toggle,
       self._long_maneuver_toggle,
       self._lat_maneuver_toggle,
       self._alpha_long_toggle,
@@ -152,6 +163,16 @@ class DeveloperLayoutMici(NavScroller):
     self._long_maneuver_toggle.set_checked(False)
     ui_state.params.put_bool("LateralManeuverMode", False)
     self._lat_maneuver_toggle.set_checked(False)
+
+  def _on_start_can_bridge(self, state: bool):
+    global _bridge_proc
+    if state:
+      if _bridge_proc is None or _bridge_proc.poll() is not None:
+        _bridge_proc = subprocess.Popen([BRIDGE_PATH, "can"])
+    else:
+      if _bridge_proc is not None and _bridge_proc.poll() is None:
+        _bridge_proc.send_signal(signal.SIGTERM)
+        _bridge_proc = None
 
   def _on_long_maneuver_mode(self, state: bool):
     ui_state.params.put_bool("LongitudinalManeuverMode", state)
